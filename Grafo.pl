68677f8160f0d70be0415c10b179b59198d5965b
sebasqr22 <63075293+sebasqr22@users.noreply.github.com>
2022-03-21 16:03:55 -0600

    Se cambian algunos errores que presentaba el grafo
    Co-Authored-By: Mario <48070830+mjcarranza@users.noreply.github.com>

--- Grafo.pl
+++ Grafo.pl
@@ -1,60 +1,85 @@
 %arco(aerolinea, vuelo, AtoDestino, TiempoEstimado, Clase, Costo)
 %consult("/Users/sebastianqr.2208/Documents/GitHub/TravelAgencyLog/prueba.pl").
-arco('COPA ', 'CM404 ', 'SJO ', 'PTY ', 1.0, 'ambas ', 300).%panama
-arco('AVIANCA ', 'DL7606 ', 'SJO ', 'PTY ', 1.0, 'económica ', 280).%panama
 
-arco('AMERICAN ', 'AA1353 ', 'SJO ', 'MIA ', 2.45, 'de negocios ', 400). %miami
-arco('UNITED ', 'AF4214 ', 'SJO ', 'MIA ', 2.45, 'económica ', 390). %miami
+%lugar(paÃ­s, aeropuerto)
+lugar(costarica, sjo).
+lugar(mexico, mex).
+lugar(panama, pty).
+lugar(chile, scl).
+lugar(colombia, mde).
+lugar(brazil, gru).
+lugar(orlando, mco).
+lugar(miami, mia).
+lugar(nuevaYork, jfk).
 
-arco('AMERICAN ', 'AA865 ', 'SJO ', 'JFK ', 5.15, 'económica ', 500). %ny
-arco('AVIANCA ', 'AU2412 ', 'SJO ', 'JFK ', 5.15, 'económica ', 505). %ny
+%clases
+clase(economica).
+clase(negocios).
+clase(ambas).
 
-arco('AEROMEXICO ', 'AM656 ', 'SJO ', 'MEX ', 3.0, 'de negocios ', 300). %mex
-arco('AVIANCA ', 'AM656 ', 'SJO ', 'MEX ', 3.0, 'ambas ', 300). %mex
+%Aerolineas
+aerolinea(copa).
+aerolinea(avianca).
+aerolinea(american).
+aerolinea(united).
+aerolinea(aeromexico).
+aerolinea(latam).
 
-arco('COPA ', 'CM759 ', 'PTY ', 'GRU ', 7, 'ambas ', 1000). %brazil sao pablo
-arco('LATAM ', 'AZ681', 'PTY', 'GRU', 7, 'ambas', 1050). %brazil sao pablo
+arco(copa, cm404, sjo, pty, 1.0, ambas , 300).%panama
+arco(avianca, dl7606 , sjo, pty, 1.0, economica , 280).%panama
 
-arco('COPA ', 'CM432 ', 'PTY ', 'MEX ', 4.10, 'económica ', 400).
-arco('UNITED ', 'CM432 ', 'PTY ', 'MEX ', 4.10, 'de negocios ', 500).
+arco(american, aa1353 , sjo, mia, 2.45, de negocios , 400). %miami
+arco(united, af4214 , sjo, mia, 2.45, economica , 390). %miami
 
-arco('COPA ', 'CM150 ', 'PTY ', 'MDE ', 0.40, 'de negocios ', 200). %medellin colombia
-arco('AVIANCA ', 'AR707 ', 'PTY ', 'MDE ', 0.40, 'de negocios ', 230). %medellin colombia
+arco(american, aa865 , sjo, jfk, 5.15, economica , 500). %ny
+arco(avianca, au2412 , sjo, jfk, 5.15, economica , 505). %ny
 
-arco('COPA ', 'CM446 ', 'PTY ', 'MCO ', 3.40, 'ambas ', 420). %orlando
-arco('AMERICAN ', 'UX042 ', 'PTY ', 'MCO ', 3.40, 'económica ', 420). %orlando
+arco(aeromexico, am656 , sjo, mex, 3.0, de negocios , 300). %mex
+arco(avianca, am656 , sjo, mex, 3.0, ambas , 300). %mex
 
-arco('COPA ', 'CM277 ', 'PTY ', 'SCL ', 6.30, 'ambas ', 700). %chile santiago
-arco('UNITED ', 'JJ8012 ', 'PTY ', 'SCL ', 6.30, 'ambas ', 700). %chile santiago
+arco(copa, cm759 , pty, gru, 7, ambas , 1000). %brazil sao pablo
+arco(latam, az681, pty, gru, 7, ambas, 1050). %brazil sao pablo
+
+arco(copa, cm432 , pty, mex, 4.10, economica , 400).
+arco(united, cm432 , pty, mex, 4.10, de negocios , 500).
+
+arco(copa, cm150 , pty, mde, 0.40, de negocios , 200). %medellin colombia
+arco(avianca, ar707 , pty, mde, 0.40, de negocios , 230). %medellin colombia
+
+arco(copa, cm446 , pty, mco, 3.40, ambas , 420). %orlando
+arco(american, ux042 , pty, mco, 3.40, economica , 420). %orlando
+
+arco(copa, cm277 , pty, scl, 6.30, ambas , 700). %chile santiago
+arco(united, jj8012 , pty, scl, 6.30, ambas , 700). %chile santiago
 
 
 %vuelos de vuelta
-arco('COPA ', 'CM405 ', 'PTY ', 'SJO ', 1.0, 'Ambas ', 300).
-arco('AVIANCA ', 'DL7606 ', 'PTY ', 'SJO ', 1.0, 'económica ', 280).%panama
+arco(copa, cm405 , pty, sjo, 1.0, ambas , 300).
+arco(avianca, dl7606 , pty, sjo, 1.0, economica , 280).%panama
 
-arco('AMERICAN ', 'AA1354 ', 'MIA ', 'SJO ', 2.45, 'de negocios', 400). %miami
-arco('UNITED ', 'AF4214 ', 'MIA ', 'SJO ', 2.45, 'económica ', 390). %miami
+arco(american, aa1354 , mia, sjo, 2.45, de negocios, 400). %miami
+arco(united, af4214 , mia, sjo, 2.45, economica , 390). %miami
 
-arco('AMERICAN', 'AA866', 'JFK', 'SJO', 5.15, 'económica ', 500). %ny
-arco('AVIANCA ', 'AU2412 ', 'JFK ', 'SJO ', 5.15, 'económica ', 505). %ny
+arco(american, aa866, jfk, sjo, 5.15, economica , 500). %ny
+arco(avianca, au2412 , jfk, sjo, 5.15, economica , 505). %ny
 
-arco('AEROMEXICO', 'AM657', 'MEX', 'SJO', 3.0, 'de necocios ', 300).
-arco('AVIANCA ', 'AM656 ', 'SJO ', 'MEX ', 3.0, 'ambas ', 300). %mex
+arco(aeromexico, am657, mex, sjo, 3.0, de negocios , 300).
+arco(avianca, am656 , sjo, mex, 3.0, ambas , 300). %mex
 
-arco('COPA', 'CM760', 'GRU', 'PTY', 7, '', 1000). %brazil sao pablo
-arco('LATAM ', 'AZ681', 'GRU', 'PTY', 7, 'ambas', 1050). %brazil sao pablo
+arcocopa, cm760, gru, pty, 7, de negocios, 1000). %brazil sao pablo
+arco(latam, az681, gru, pty, 7, ambas, 1050). %brazil sao pablo
 
-arco('COPA', 'CM433', 'MEX', 'PTY', 4.10, '', 400).
-arco('UNITED ', 'CM432 ', 'MEX ', 'PTY ', 4.10, 'de negocios ', 500).
+arcocopa, cm433, mex, pty, 4.10, economica, 400).
+arco(united, cm432 , mex, pty, 4.10, de negocios , 500).
 
-arco('COPA', 'CM151', 'MDE', 'PTY', 0.40, '', 200). %medellin colombia
-arco('AVIANCA ', 'AR707 ', 'MDE ', 'PTY ', 0.40, 'de negocios ', 230). %medellin colombia
+arcocopa, cm151, mde, pty, 0.40, economica, 200). %medellin colombia
+arco(avianca, ar707 , mde, pty, 0.40, de negocios , 230). %medellin colombia
 
-arco('COPA', 'CM445', 'MCO', 'PTY', 3.40, '', 420). %orlando
-arco('AMERICAN ', 'UX042 ', 'MCO ', 'PTY ', 3.40, 'económica ', 420). %orlando
+arco(copa, cm445, mco, pty, 3.40, de negocios, 420). %orlando
+arco(american, ux042 , mco, pty, 3.40, economica , 420). %orlando
 
-arco('COPA', 'CM278', 'SCL', 'PTY', 6.30, '', 700). %chile santiago
-arco('UNITED ', 'JJ8012 ', 'SCL ', 'PTY ', 6.30, 'ambas ', 700). %chile santiago
+arco(copa, cm278, scl, pty, 6.30, de negocios, 700). %chile santiago
+arco(united, jj8012 , scl, pty, 6.30, ambas , 700). %chile santiago
 
 
 c(X):-arco(X, _, _, _, _, _, _).
